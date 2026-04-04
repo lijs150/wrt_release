4fe17d6de9a6d97f8338a975ae1e09b6960783ee
lijs150 <15017561044@139.com>
2026-04-04 22:14:31 +0800

    Update feeds.sh
    Signed-off-by: lijs150 <15017561044@139.com>

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -28,16 +28,6 @@ update_feeds() {
         echo 'src-git luci_app_bandix https://github.com/timsaya/luci-app-bandix.git;main' >>"$BUILD_DIR/$FEEDS_CONF"
     fi
 
-    if ! grep -q "nikki" "$BUILD_DIR/$FEEDS_CONF"; then
-        [ -z "$(tail -c 1 "$BUILD_DIR/$FEEDS_CONF")" ] || echo "" >>"$BUILD_DIR/$FEEDS_CONF"
-        echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>"$BUILD_DIR/$FEEDS_CONF"
-    fi
-
-    if ! grep -q "openlist" "$BUILD_DIR/$FEEDS_CONF"; then
-        [ -z "$(tail -c 1 "$BUILD_DIR/$FEEDS_CONF")" ] || echo "" >>"$BUILD_DIR/$FEEDS_CONF"
-        echo 'src-git openlist https://github.com/OpenListTeam/OpenList-OpenWRT;main' >>"$BUILD_DIR/$FEEDS_CONF"
-    fi
-
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
         touch "$BUILD_DIR/include/bpf.mk"
     fi
@@ -54,10 +44,6 @@ install_feeds() {
                 install_fullconenat
             elif [[ $(basename "$dir") == "passwall" ]]; then
                 install_passwall
-            elif [[ $(basename "$dir") == "nikki" ]]; then
-                install_nikki
-            elif [[ $(basename "$dir") == "openlist" ]]; then
-                install_openlist
             else
                 ./scripts/feeds install -f -ap $(basename "$dir")
             fi
